8ed70d683c3e3f0e0d777bbaeea6c2215a2b4c46
Stanislav Vetlovskiy <mrerliz@gmail.com>
2013-11-02 02:00:11 +0400

    Add package.json & grunt w/ stylus

---
+++ gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        stylus: {
+            compile: {
+                files: {
+                    'public/css/style.css': ['public/stylus/all.styl']
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['public/stylus/*.styl'],
+                tasks: ['stylus']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['stylus', 'watch']);
+}
